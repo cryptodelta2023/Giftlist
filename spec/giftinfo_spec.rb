e94b8893dfdfd78868d2b8ea241104e2edd95095
Anita <yachi77128230@gmail.com>
2023-04-13 23:31:06 +0800

    test available

--- spec/giftinfo_spec.rb
+++ spec/giftinfo_spec.rb
@@ -2,56 +2,68 @@
 
 require_relative './spec_helper'
 
-describe 'Test GiftList Handling' do
+describe 'Test Giftinfo Handling' do
   include Rack::Test::Methods
 
   before do
     wipe_database
+
+    DATA[:giftlists].each do |giftlist_data|
+      GiftListApp::Giftlist.create(giftlist_data)
+    end
   end
 
-  it 'HAPPY: should be able to get list of all giftlists' do
-    GiftListApp::Giftlist.create(DATA[:giftlists][0]).save
-    GiftListApp::Giftlist.create(DATA[:giftlists][1]).save
-    
-    get 'api/v1/giftlists'
+  it 'HAPPY: should be able to get list of all giftinfos' do
+    giftlist = GiftListApp::Giftlist.first
+    DATA[:giftinfos].each do |info|
+      giftlist.add_giftinfo(info)
+    end
+
+    get "api/v1/giftlists/#{giftlist.id}/giftinfos"
     _(last_response.status).must_equal 200
 
     result = JSON.parse last_response.body
     _(result['data'].count).must_equal 2
   end
 
-  it 'HAPPY: should be able to get details of a single giftlist' do
-    existing_list = DATA[:giftlists][1]
-    GiftListApp::Giftlist.create(existing_list).save
-    id = GiftListApp::Giftlist.first.id
+  it 'HAPPY: should be able to get details of a single giftinfo' do
+    info_data = DATA[:giftinfos][1]
+    giftlist = GiftListApp::Giftlist.first
+    info = giftlist.add_giftinfo(info_data).save
 
-    get "/api/v1/giftlists/#{id}"
+    get "/api/v1/giftlists/#{giftlist.id}/giftinfos/#{info.id}"
     _(last_response.status).must_equal 200
 
     result = JSON.parse last_response.body
-    _(result['data']['attributes']['id']).must_equal id
-    _(result['data']['attributes']['name']).must_equal existing_list['name']
+    _(result['data']['attributes']['id']).must_equal info.id
+    _(result['data']['attributes']['giftname']).must_equal info_data['giftname']
+    _(result['data']['attributes']['url']).must_equal info_data['url']
+    _(result['data']['attributes']['description']).must_equal info_data['description']
   end
 
-  it 'SAD: should return error if unknown giftlist requested' do
-    get '/api/v1/giftlists/foobar'
+  it 'SAD: should return error if unknown giftinfo requested' do
+    proj = GiftListApp::Giftlist.first
+    get "/api/v1/giftlists/#{proj.id}/documents/foobar"
 
     _(last_response.status).must_equal 404
   end
 
-  it 'HAPPY: should be able to create new giftlists' do
-    existing_list = DATA[:giftlists][1]
+  it 'HAPPY: should be able to create new giftinfos' do
+    giftlist = GiftListApp::Giftlist.first
+    info_data = DATA[:giftinfos][1]
 
     req_header = { 'CONTENT_TYPE' => 'application/json' }
-    post 'api/v1/giftlists', existing_list.to_json, req_header
+    post "api/v1/giftlists/#{giftlist.id}/giftinfos",
+         info_data.to_json, req_header
     _(last_response.status).must_equal 201
     _(last_response.header['Location'].size).must_be :>, 0
 
     created = JSON.parse(last_response.body)['data']['data']['attributes']
-    proj = GiftListApp::Giftlist.first
+    info = GiftListApp::Giftinfo.first
 
-    _(created['id']).must_equal proj.id
-    _(created['name']).must_equal existing_list['name']
-    _(created['repo_url']).must_equal existing_list['repo_url']
+    _(created['id']).must_equal info.id
+    _(created['giftname']).must_equal info_data['giftname']
+    _(created['url']).must_equal info_data['url']
+    _(created['description']).must_equal info_data['description']
   end
 end
--- spec/giftlist_spec.rb
+++ spec/giftlist_spec.rb
@@ -2,65 +2,57 @@
 
 require_relative './spec_helper'
 
-describe 'Test GiftList Handling' do
+describe 'Test Giftlist Handling' do
   include Rack::Test::Methods
 
   before do
     wipe_database
-
-    DATA[:giftlists].each do |giftlist_data|
-        GiftListApp::Giftlist.create(giftlist_data)
-    end
   end
 
-  it 'HAPPY: should be able to get list of all giftinfos' do
-    proj = GiftListApp::Giftlist.first
-    DATA[:giftinfos].each do |doc|
-      proj.add_giftinfo(doc)
-    end
+  it 'HAPPY: should be able to get list of all giftlists' do
+    GiftListApp::Giftlist.create(DATA[:giftlists][0]).save
+    GiftListApp::Giftlist.create(DATA[:giftlists][1]).save
 
-    get "api/v1/giftlists/#{proj.id}/giftinfos"
+    get 'api/v1/giftlists'
     _(last_response.status).must_equal 200
 
     result = JSON.parse last_response.body
     _(result['data'].count).must_equal 2
   end
 
-  it 'HAPPY: should be able to get details of a single giftinfo' do
-    doc_data = DATA[:giftinfos][1]
-    proj = GiftListApp::Giftlist.first
-    doc = proj.add_giftinfo(doc_data).save
+  it 'HAPPY: should be able to get details of a single giftlist' do
+    existing_proj = DATA[:giftlists][1]
+    GiftListApp::Giftlist.create(existing_proj).save
+    id = GiftListApp::Giftlist.first.id
 
-    get "/api/v1/giftlists/#{proj.id}/giftinfos/#{doc.id}"
+    get "/api/v1/giftlists/#{id}"
     _(last_response.status).must_equal 200
 
     result = JSON.parse last_response.body
-    _(result['data']['attributes']['id']).must_equal doc.id
-    _(result['data']['attributes']['filename']).must_equal doc_data['filename']
+    _(result['data']['attributes']['id']).must_equal id
+    _(result['data']['attributes']['list_name']).must_equal existing_proj['list_name']
+    _(result['data']['attributes']['list_owner']).must_equal existing_proj['list_owner']
   end
 
-  it 'SAD: should return error if unknown giftinfo requested' do
-    proj = GiftListApp::Giftlist.first
-    get "/api/v1/giftlists/#{proj.id}/giftinfos/foobar"
+  it 'SAD: should return error if unknown giftlist requested' do
+    get '/api/v1/giftlists/foobar'
 
     _(last_response.status).must_equal 404
   end
 
-  it 'HAPPY: should be able to create new giftinfos' do
-    proj = GiftListApp::Giftlist.first
-    doc_data = DATA[:giftinfos][1]
+  it 'HAPPY: should be able to create new giftlists' do
+    existing_proj = DATA[:giftlists][1]
 
     req_header = { 'CONTENT_TYPE' => 'application/json' }
-    post "api/v1/giftlists/#{proj.id}/giftinfos",
-         doc_data.to_json, req_header
+    post 'api/v1/giftlists', existing_proj.to_json, req_header
     _(last_response.status).must_equal 201
     _(last_response.header['Location'].size).must_be :>, 0
 
     created = JSON.parse(last_response.body)['data']['data']['attributes']
-    doc = GiftListApp::Giftinfo.first
+    proj = GiftListApp::Giftlist.first
 
-    _(created['id']).must_equal doc.id
-    _(created['filename']).must_equal doc_data['filename']
-    _(created['description']).must_equal doc_data['description']
+    _(created['id']).must_equal proj.id
+    _(created['list_name']).must_equal existing_proj['list_name']
+    _(created['list_owner']).must_equal existing_proj['list_owner']
   end
 end
