b10b515b7bcf0f7a7164efbfa77a410f02c919af
Anita <yachi77128230@gmail.com>
2023-04-13 23:33:54 +0800

    test available

--- spec/giftinfo_spec.rb
+++ spec/giftinfo_spec.rb
@@ -43,7 +43,7 @@ describe 'Test Giftinfo Handling' do
 
   it 'SAD: should return error if unknown giftinfo requested' do
     proj = GiftListApp::Giftlist.first
-    get "/api/v1/giftlists/#{proj.id}/documents/foobar"
+    get "/api/v1/giftlists/#{proj.id}/giftinfos/foobar"
 
     _(last_response.status).must_equal 404
   end
