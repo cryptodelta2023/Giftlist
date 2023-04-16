7c40b9cd5b4fd4916cec95d99f7e44a04f388c73
Fong <fong54388@gmail.com>
2023-04-16 23:13:02 +0800

    Adjust new architechure of spec file  for db-hardening

--- spec/integration/api_giftinfos_spec.rb
+++ spec/integration/api_giftinfos_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative './spec_helper'
+require_relative '../spec_helper'
 
 describe 'Test Giftinfo Handling' do
   include Rack::Test::Methods
@@ -29,7 +29,7 @@ describe 'Test Giftinfo Handling' do
   it 'HAPPY: should be able to get details of a single giftinfo' do
     info_data = DATA[:giftinfos][1]
     giftlist = GiftListApp::Giftlist.first
-    info = giftlist.add_giftinfo(info_data).save
+    info = giftlist.add_giftinfo(info_data)
 
     get "/api/v1/giftlists/#{giftlist.id}/giftinfos/#{info.id}"
     _(last_response.status).must_equal 200
@@ -48,22 +48,37 @@ describe 'Test Giftinfo Handling' do
     _(last_response.status).must_equal 404
   end
 
-  it 'HAPPY: should be able to create new giftinfos' do
-    giftlist = GiftListApp::Giftlist.first
-    info_data = DATA[:giftinfos][1]
+  describe 'Creating Giftinfos' do
+    before do
+      @giftlist = GiftListApp::Giftlist.first
+      @info_data = DATA[:giftinfos][1]
+      @req_header = { 'CONTENT_TYPE' => 'application/json' }
+    end
 
-    req_header = { 'CONTENT_TYPE' => 'application/json' }
-    post "api/v1/giftlists/#{giftlist.id}/giftinfos",
-         info_data.to_json, req_header
-    _(last_response.status).must_equal 201
-    _(last_response.header['Location'].size).must_be :>, 0
+    it 'HAPPY: should be able to create new documents' do
+      req_header = { 'CONTENT_TYPE' => 'application/json' }
+      post "api/v1/giftlists/#{giftlist.id}/giftinfos",
+           info_data.to_json, req_header
+      _(last_response.status).must_equal 201
+      _(last_response.header['Location'].size).must_be :>, 0
 
-    created = JSON.parse(last_response.body)['data']['data']['attributes']
-    info = GiftListApp::Giftinfo.first
+      created = JSON.parse(last_response.body)['data']['data']['attributes']
+      info = GiftListApp::Giftinfo.first
 
-    _(created['id']).must_equal info.id
-    _(created['giftname']).must_equal info_data['giftname']
-    _(created['url']).must_equal info_data['url']
-    _(created['description']).must_equal info_data['description']
+      _(created['id']).must_equal info.id
+      _(created['giftname']).must_equal info_data['giftname']
+      _(created['url']).must_equal info_data['url']
+      _(created['description']).must_equal info_data['description']
+    end
+
+    it 'SECURITY: should not create documents with mass assignment' do
+      bad_data = @info_data.clone
+      bad_data['created_at'] = '1900-01-01'
+      post "api/v1/projects/#{@giftlist.id}/giftinfos",
+           bad_data.to_json, @req_header
+
+      _(last_response.status).must_equal 400
+      _(last_response.headers['Location']).must_be_nil
+    end
   end
 end
